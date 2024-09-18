d753663babb738e66a57543d3d1f1eb41d81aea9
zubeyranwar <zubeyranwar@gmail.com>
2024-09-18 09:58:49 +0300

    feat: make the test url exportable

--- src/redux/api/util.js
+++ src/redux/api/util.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://mapapi.gebeta.app";
 export const LOCAL_BASE_URL = "http://localhost:8081/api";
-const TEST_URL = "https://apitest.gebeta.app";
+export const TEST_URL = "https://apitest.gebeta.app";
 // http://68.219.183.24:8080
 
 export const API = axios.create({
