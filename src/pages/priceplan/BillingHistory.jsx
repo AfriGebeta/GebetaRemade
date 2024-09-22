13f7d0f7e12c9a692e52215eb008ff61d40f0940
zubeyranwar <zubeyranwar@gmail.com>
2024-09-22 20:30:38 +0300

    feat: fetching billing history

--- src/pages/priceplan/BillingHistory.jsx
+++ src/pages/priceplan/BillingHistory.jsx
@@ -1,35 +1,71 @@
+import { useEffect, useState } from "react";
+import { getAllBilling } from "../../redux/api/billingAPI";
+import { useSelector } from "react-redux";
+import { getCredit } from "../../redux/api/creditsApi";
+import billing from '../../assets/img/billing.png'
+
 function BillingHistory() {
-    const formatter = new Intl.NumberFormat('en-US');
+    const user = useSelector(state => state.user)
+
+    const [billingData, setBillingData] = useState([])
+
+    function formatDate(inputDate) {
+        const date = new Date(inputDate);
+        const day = String(date.getDate()).padStart(2, '0');
+        const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+        const year = String(date.getFullYear()).slice(-2); // Get last two digits of the year
+
+        return `${month}/${day}/${year}`;
+    }
+
+    useEffect(() => {
+        async function getBillingData() {
+            const response = await getAllBilling(user.data.token)
+            console.log(response)
+            setBillingData(response)
+        }
+
+        getBillingData()
+    }, [])
+
+    console.log(billingData)
 
     return (
-        <div className='bg-[#202022] text-[#ccc] p-4 rounded-lg'>
-            <h2 className='text-xl text-white font-semibold mb-4'>Billing History</h2>
-            <div className="space-y-4">
-                {data.map((item, index) => (
-                    <div key={index} className='flex flex-col gap-2 pb-4 border-b border-gray-700 last:border-b-0'>
-                        <div className='flex justify-between items-center'>
-                            <h4 className='text-base text-white font-semibold'>{item.name} Pac.</h4>
-                            <h5 className='text-[#ccc] text-xs font-medium'>{item.date}</h5>
-                        </div>
-                        <div className='flex items-center gap-2'>
-                            <span className='bg-green-700 w-1 h-4'></span>
-                            <h4 className='text-sm font-semibold text-GebetaMain'>ETB {item.price}</h4>
-                        </div>
-                        <p className="pb-1 text-sm">{item.startDate} - {item.endDate}</p>
-                        <div>
-                            <p className='font-semibold text-md'>{item.name} package</p>
-                            <p className="mt-0">{formatter.format(item.calls)} calls</p>
-                        </div>
-                        <div className='flex justify-between text-sm'>
-                            <h4 className="font-semibold">Payment Status</h4>
-                            {item.status ?
-                                <h4 className='text-green-500 font-semibold'>Completed</h4> :
-                                <h4 className='text-GebetaMain font-semibold'>Pending</h4>
-                            }
-                        </div>
+        <div className='bg-[#202022] text-[#ccc] p-4 rounded-lg min-h-[60vh]'>
+            {billingData?.length > 0 ?
+                <>
+                    <h2 className='text-xl text-white font-semibold mb-4'>Billing History</h2>
+                    <div className="space-y-4">
+                        {billingData?.map((item, index) => (
+                            <div key={index} className='flex flex-col gap-6 pb-4 border-b border-gray-700 last:border-b-0'>
+                                <div className='flex justify-between items-center'>
+                                    <h4 className='text-base text-white font-semibold'>Business Pac.</h4>
+                                    <h5 className='text-[#ccc] text-xs font-medium'>{formatDate(item.created_at)}</h5>
+                                </div>
+                                <div className='flex items-center gap-2'>
+                                    <span className='bg-green-700 w-1 h-4'></span>
+                                    <h4 className='text-sm font-semibold text-GebetaMain'>{item.amount_due} ETB</h4>
+                                </div>
+                                <div className='flex justify-between text-sm'>
+                                    <h4 className="font-semibold">Payment Status</h4>
+                                    {item?.installments?.map((installment) => (
+                                        <>
+                                            <h4 className='text-green-500 font-semibold'>{installment.method}</h4>
+                                            <h4 className='text-GebetaMain font-semibold'>{installment.status}</h4>
+                                        </>
+                                    ))
+                                    }
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
+                </>
+                : (
+                    <div className="flex flex-col justify-center text-center">
+                        <img className="w-full h-[250px] text-GebetaMain" src={billing} alt="billing image"/>
+                        <p>No Billing History</p>
+                    </div>
+                )}
         </div>
     )
 }
