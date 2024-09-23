db6d1176626196d6da577c99bfa84b99c93cd483
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 11:04:54 +0300

    feat: change way of fetching to react query

--- src/pages/priceplan/index.jsx
+++ src/pages/priceplan/index.jsx
@@ -9,24 +9,13 @@ import ApiToken from "./";
 
 
 function Priceplan() {
-    const [metrics, setMetrics] = useState({})
-    const user = useSelector((state) => state).user
-
-    useEffect(() => {
-        const controller = new AbortController()
-        getUserUsage(user.data.token, controller).then((response) => {
-            if (response.error == null) setMetrics(response.data.data)
-        })
-
-        return () => controller.abort()
-    }, [])
 
     return (
         <div className="bg-Dark min-h-screen">
             <div className="container mx-auto px-6 py-8">
                 <div className="grid grid-cols-1 text-[#ccc] lg:grid-cols-3 gap-4">
                     <div className="lg:col-span-2 space-y-4">
-                        <ApiDetail metrics={metrics} />
+                        <ApiDetail />
                         <Plans />
                     </div>
                     <div className="lg:col-span-1">
