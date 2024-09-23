6e2e2cf7e3c3ccf0d451566c3fec15e7c7da5667
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 11:58:33 +0300

    feat: add billing api

---
+++ src/redux/api/billingAPI.js
@@ -0,0 +1,18 @@
+import { API, handleApiError } from "./util";
+
+export const getAllBilling = async (apiToken) => {
+  try {
+    const response = await API.get("/api/sales/get-all", {
+      params: {
+        page: 1,
+        limit: 10,
+      },
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    });
+    return response.data.data.places;
+  } catch (error) {
+    return handleApiError(error);
+  }
+};
