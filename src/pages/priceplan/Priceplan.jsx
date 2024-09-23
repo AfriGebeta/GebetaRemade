7ec7dbff1bd9ed1da8ca2a4373e11940912464b8
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 11:06:11 +0300

    feat: change way of fetching to react query

--- src/pages/priceplan/Priceplan.jsx
+++ src/pages/priceplan/Priceplan.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { getAllCredits } from "../../redux/api/creditsApi";
-import { buyCredit } from "../../redux/api/buyCreditApi";
-import { FaCheck } from "react-icons/fa6";
+import React, {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
+import {getAllCredits} from "../../redux/api/creditsApi";
+import {buyCredit} from "../../redux/api/buyCreditApi";
+import {FaCheck} from "react-icons/fa6";
+import {useQuery} from "@tanstack/react-query";
 
-function Plan({ data, index }) {
+function Plan({data, index}) {
     const user = useSelector((state) => state).user;
 
     const handleUpgrade = () => {
@@ -17,10 +18,12 @@ function Plan({ data, index }) {
             .catch(err => console.log(err));
     };
 
-    const isPurchased = data.name != 'Custom' ? user.data?.user?.credits?.find(item=>item.bundle_id ==data.id) : false
+    const isPurchased = data.name != 'Custom' ? user.data?.user?.credits?.find(item => item.bundle_id == data.id) : false
     const isLogin = !!user.data.token
     return (
-        <div className={`relative flex flex-col gap-3 text-white ${isPurchased && data.name!='Custom' ? 'bg-GebetaMain/30' : 'bg-[#202022]'} p-6 h-[420px] rounded-lg`} key={index}>
+        <div
+            className={`relative flex flex-col gap-3 text-white ${isPurchased && data.name != 'Custom' ? 'bg-GebetaMain/30' : 'bg-[#202022]'} p-6 h-[420px] rounded-lg`}
+            key={index}>
             <div className="relative">
                 <h3 className="font-bold text-lg mb-4">{data.name}</h3>
                 {data.name !== 'Custom' ? (
@@ -38,18 +41,20 @@ function Plan({ data, index }) {
                     </div>
 
                 )}
-                {data.name !== 'Custom' && <span className="absolute top-1 right-2 bg-green-600 inline-block px-2 py-1 rounded text-xs font-semibold">
+                {data.name !== 'Custom' && <span
+                    className="absolute top-1 right-2 bg-green-600 inline-block px-2 py-1 rounded text-xs font-semibold">
                     {data.expiredIn} days
                 </span>}
-                <hr className={`${data.name !== 'Custom' ? "mt-3" : "mt-6"} mb-3 border-gray-600`} />
+                <hr className={`${data.name !== 'Custom' ? "mt-3" : "mt-6"} mb-3 border-gray-600`}/>
             </div>
 
             <div className="flex-grow">
                 <ul className="space-y-1">
                     {data.included_call_types.map((feature, i) => (
                         <li className="flex items-center space-x-1 text-xs py-1" key={i}>
-                            <FaCheck className="block text-sm text-green-400 mr-2 shrink-0" />
-                            <span className='font-medium text-sm'>{data.call_caps[i] || '0'} {feature.charAt(0).toUpperCase() + feature.slice(1).toLowerCase()} calls</span>
+                            <FaCheck className="block text-sm text-green-400 mr-2 shrink-0"/>
+                            <span
+                                className='font-medium text-sm'>{data.call_caps[i] || '0'} {feature.charAt(0).toUpperCase() + feature.slice(1).toLowerCase()} calls</span>
                         </li>
                     ))}
                 </ul>
@@ -57,12 +62,12 @@ function Plan({ data, index }) {
 
             <div className="mt-2">
                 {data.name != "Custom" ? <button
-                    className={`w-full transition duration-150 border border-zinc-700 borer-2 outline-none ${isPurchased ? 'bg-white text-zinc-950 border-none' : 'hover:bg-GebetaMain'} hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium`}
-                    onClick={handleUpgrade}
-                    disabled={isPurchased}
-                >
-                    {isPurchased ?  `Selected Plan` : isLogin ? 'Upgrade' : 'Buy Now'}
-                </button>
+                        className={`w-full transition duration-150 border border-zinc-700 borer-2 outline-none ${isPurchased ? 'bg-white text-zinc-950 border-none' : 'hover:bg-GebetaMain'} hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium`}
+                        onClick={handleUpgrade}
+                        disabled={isPurchased}
+                    >
+                        {isPurchased ? `Selected Plan` : isLogin ? 'Upgrade' : 'Buy Now'}
+                    </button>
                     :
                     <button
                         className='w-full transition duration-150 border border-gray-600 outline-none hover:bg-GebetaMain hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium'
@@ -95,28 +100,32 @@ function Plans() {
         ],
     };
 
-    const [credits, setCredits] = useState([]);
+    const SkeletonCard = () => (
+        <div
+            className="w-full p-6 h-[420px] rounded-lg bg-[#202022] rounded-lg shadow-md p-4 flex flex-col justify-between items-center min-w-[200px] animate-pulse">
+            <div>
+                {Array(5).map((_, i) => (
+                    <div className="h-5 w-3/4 bg-gray-700 rounded mb-4"></div>
+                ))}
+            </div>
+        </div>
+    );
 
-    useEffect(() => {
-        const controller = new AbortController();
-
-        getAllCredits({ page: 1, limit: 10 }, controller)
-            .then((response) => {
-                setCredits(response.data.data.data);
-            })
-            .catch(err => {
-                console.log(err);
-            });
-
-        return () => controller.abort();
-    }, []);
+    const {data, isLoading} = useQuery({
+        queryKey: ['plans'],
+        queryFn: () => getAllCredits({page: 1, limit: 10})
+    })
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
-            {credits?.map((credit, index) => (
-                <Plan data={credit} index={index} key={index} />
-            ))}
-            <Plan data={enterprise} index={credits.length} /> {/* Include enterprise plan */}
+            {isLoading ?
+                Array(5).fill(0).map((_, i) => <SkeletonCard key={i}/>) :
+                data?.map((credit, index) => (
+                    <Plan data={credit} index={index} key={index}/>
+                ))}
+
+
+            <Plan data={enterprise} index={data?.length || 1}/> {/* Include enterprise plan */}
         </div>
     );
 }
