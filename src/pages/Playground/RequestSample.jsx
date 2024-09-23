f8d9701f365dade1c204b3dd1f31f116f636d232
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 07:07:56 +0300

    feat: change background color and text size

--- src/pages/Playground/RequestSample.jsx
+++ src/pages/Playground/RequestSample.jsx
@@ -5,10 +5,9 @@ const RequestSample = (props) => {
   const handlerequestLanguage = (i) => setRequestLanguage(i)
   return (
     <div className='mt-[10%] mx-[2%] md:mx-[0%]'>
-    <p className='mt-[2%] font-bold text-xl text-[#A0AABA]'>Request samples</p>
-    <div className='bg-[#11171a] text-[#A0AABA] mt-[2%] py-[1%] mb-[1%]' style={{ height: requestLanguage === 0 ? 'auto' : '300px'}}>
-          <p className='mx-[2%] space-x-2 pb-[1%] break-all'>{props.curl}</p> 
-  
+    <p className='mt-[2%] font-bold text-lg text-zinc-600 mb-2'>Request samples</p>
+    <div className='border-grey-500 bg-zinc-100 text-zinc-800  mt-[2%] py-[1%] mb-[1%]' style={{ height: requestLanguage === 0 ? 'auto' : '300px'}}>
+          <p className='mx-[2%] space-x-2 pb-[1%] break-all text-sm'>{props.curl}</p>
     </div>
   </div>
   )
--- src/pages/Playground/Responsesample.jsx
+++ src/pages/Playground/Responsesample.jsx
@@ -4,11 +4,10 @@ const ResponseSample = (props) => {
   const [requestLanguage, setRequestLanguage] = useState(0)
   const handlerequestLanguage = (i) => setRequestLanguage(i)
   return (
-    <div className='mt-[15%] h-[50%] mx-[2%] md:mx-[0%]'>
-    <p className='mt-[2%] font-bold text-xl text-[#A0AABA]'>Response samples</p>
-    <div className='bg-[#11171a] text-[#A0AABA] mt-[2%] py-[1%] mb-[1%]' style={{ height: requestLanguage === 0 ? 'auto' : '300px'}}>
+    <div className='mt-[10%] h-[50%] mx-[2%] md:mx-[0%]'>
+    <p className='mt-[2%] font-bold text-lg text-zinc-600 mb-2'>Response samples</p>
+    <div className='border-grey-500 bg-zinc-100 text-zinc-800 text-sm mt-[2%] py-[1%] mb-[1%]' style={{ height: requestLanguage === 0 ? 'auto' : '300px'}}>
           {props.component}
-  
     </div>
   </div>
   )
