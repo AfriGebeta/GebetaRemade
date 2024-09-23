73c0f94f8e8fa030d4053f771e38234e29632d35
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 12:13:33 +0300

    feat: remove data from locals storage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,25 +1,28 @@
-import React, { createContext, useState } from 'react';
+import React, {createContext, useState} from 'react';
+import {persistor} from "../redux/store";
 
 export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    // Check if the user was authenticated before the page reload
-    () => JSON.parse(localStorage.getItem('isAuthenticated')) || false
-  );
- const login = () => {
-   setIsAuthenticated(true);
-   localStorage.setItem('isAuthenticated', JSON.stringify(true));
- };
+export const AuthProvider = ({children}) => {
+    const [isAuthenticated, setIsAuthenticated] = useState(
+        // Check if the user was authenticated before the page reload
+        () => JSON.parse(localStorage.getItem('isAuthenticated')) || false
+    );
+    const login = () => {
+        setIsAuthenticated(true);
+        localStorage.setItem('isAuthenticated', JSON.stringify(true));
+    };
 
- const logout = () => {
-   setIsAuthenticated(false);
-   localStorage.removeItem('isAuthenticated');
- };
+    const logout = () => {
+        setIsAuthenticated(false);
 
- return (
-   <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
-     {children}
-   </AuthContext.Provider>
- );
+        localStorage.removeItem('isAuthenticated');
+        persistor.purge()
+    };
+
+    return (
+        <AuthContext.Provider value={{isAuthenticated, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
