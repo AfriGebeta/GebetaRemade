6ff9544a4ca37c6ecee8fa6f747c16e9ce6b8378
zubeyranwar <zubeyranwar@gmail.com>
2024-09-23 10:34:18 +0300

    feat: Change fetching method to react query

--- src/component/Card/ApiDetail.jsx
+++ src/component/Card/ApiDetail.jsx
@@ -1,20 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { getCredit } from "../../redux/api/creditsApi";
+import {useQuery} from "@tanstack/react-query";
+import {getUserUsage} from "../../redux/api/usageAPI";
 
 function ApiDetail({ metrics }) {
   const user = useSelector((state) => state.user);
-  const [isLoading, setIsLoading] = useState(true);
-  const [processedMetrics, setProcessedMetrics] = useState([]);
   const [purchasedPlans, setPurchasedPlans] = useState('')
 
 
+  const {data, isLoading} = useQuery({
+    queryKey: ['metrics'],
+    queryFn: () => getUserUsage(user.data.token),
+    staleTime: 5 * 60 * 1000
+  })
+
   useEffect(() => {
-    if (user.data.user.token.length > 0) {
+    if (user?.data?.user?.token) {
       setPurchasedPlans("Credits")
     }
 
-    if (user.data.user.purchased_date != null) {
+    if (user?.data?.user?.purchased_date != null) {
       setPurchasedPlans("Pay as you go")
     }
   }, [])
@@ -27,28 +33,19 @@ function ApiDetail({ metrics }) {
     { calltype: "Geocoding", total: 0 },
   ];
 
-  useEffect(() => {
-    if (metrics && Array.isArray(metrics) && metrics.length > 0) {
-      setProcessedMetrics(metrics);
-    } else {
-      setProcessedMetrics(defaultMetrics);
-    }
-    setIsLoading(false);
-  }, [metrics]);
-
   const getTotal = () => {
-    return processedMetrics.reduce((acc, item) => acc + item.total, 0);
+    return data.reduce((acc, item) => acc + item.total, 0);
   };
 
   const getMaximum = () => {
-    let maxValue = Math.max(...processedMetrics.map(metric => metric.total));
-    let maxMessage = processedMetrics.find(item => item.total === maxValue).calltype;
+    let maxValue = Math.max(...data.map(metric => metric.total));
+    let maxMessage = data.find(item => item.total === maxValue).calltype;
     return `${maxValue} ${maxMessage.charAt(0).toUpperCase() + maxMessage.slice(1).toLowerCase()}`;
   };
 
   const getMinimum = () => {
-    let minValue = Math.min(...processedMetrics.map(metric => metric.total));
-    let minMessage = processedMetrics.find(item => item.total === minValue).calltype;
+    let minValue = Math.min(...data.map(metric => metric.total));
+    let minMessage = data.find(item => item.total === minValue).calltype;
     return `${minValue} ${minMessage.charAt(0).toUpperCase() + minMessage.slice(1).toLowerCase()}`;
   };
 
