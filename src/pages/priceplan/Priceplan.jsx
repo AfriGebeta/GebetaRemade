933373cbc4d80092cb3131ace6f9aff01196c255
zubeyranwar <zubeyranwar@gmail.com>
2024-09-24 08:13:35 +0300

    feat: redirect user to checkout

--- src/pages/priceplan/Priceplan.jsx
+++ src/pages/priceplan/Priceplan.jsx
@@ -4,80 +4,130 @@ import {getAllCredits} from "../../redux/api/creditsApi";
 import {buyCredit} from "../../redux/api/buyCreditApi";
 import {FaCheck} from "react-icons/fa6";
 import {useQuery} from "@tanstack/react-query";
+import {useNavigate} from "react-router-dom";
+import Signin from "../Signin/SignIn";
+import Modal from "../../component/Modal/Modal";
+import Signup from "../Signup/Signup";
 
 function Plan({data, index}) {
     const user = useSelector((state) => state).user;
+    const isLogin = localStorage.getItem('isAuthenticated')
+    const [signinModal, setSigninModal] = useState(false);
+    const [signupModal, setSignUpModal] = useState(false);
+    const navigate = useNavigate();
 
-    const handleUpgrade = () => {
-        buyCredit(user.data.token, data.id)
-            .then(response => {
-                if (response.data.data.status === "success") {
-                    window.open(response.data.data.Data.checkout_url, '_blank');
-                }
-            })
-            .catch(err => console.log(err));
+    const fromSignIntoSignUp = () => {
+        setSignUpModal(true);
+        setSigninModal(false);
+    };
+
+    const fromSignupIntoSignIn = () => {
+        setSignUpModal(false);
+        setSigninModal(true);
+    };
+
+    const handleUpgrade = (id) => {
+        if (!isLogin) {
+            localStorage.setItem('plan', id)
+            localStorage.setItem('redirect', true)
+            setSigninModal(true)
+
+            // buyCredit(user.data.token, id)
+            //     .then(response => {
+            //         if (response.data.data.status === "success") {
+            //             window.open(response.data.data.Data.checkout_url, '_blank');
+            //         }
+            //     })
+            //     .catch(err => console.log(err));
+        } else {
+            if (data.name !== "Custom") {
+                buyCredit(user.data.token, id)
+                    .then(response => {
+                        if (response.data.data.status === "success") {
+                            window.open(response.data.data.Data.checkout_url, '_blank');
+                        }
+                    })
+                    .catch(err => console.log(err));
+            } else {
+                navigate('/contact')
+            }
+        }
+
     };
 
     const isPurchased = data.name !== 'Custom' ? user.data?.user?.credits?.find(item => item.bundle_id === data.id) : false
-    const isLogin = localStorage.getItem('isAuthenticated')
     return (
-        <div
-            className={`relative flex flex-col gap-3 text-white ${isPurchased && data.name !== 'Custom' ? 'bg-GebetaMain/30' : 'bg-[#202022]'} p-6 h-[420px] rounded-lg`}
-            key={index}>
-            <div className="relative">
-                <h3 className="font-bold text-lg mb-4">{data.name}</h3>
-                {data.name !== 'Custom' ? (
-                    <div className="w-5 flex gap-x-2">
+        <>
+            <Modal
+                open={signinModal}
+                close={() => setSigninModal(false)}
+                elem={<Signin signintosignup={fromSignIntoSignUp}/>}
+            />
+
+            <Modal
+                open={signupModal}
+                close={() => setSignUpModal(false)}
+                elem={<Signup signupintosignin={fromSignupIntoSignIn}/>}
+            />
+
+            <div
+                className={`relative flex flex-col gap-3 text-white ${isPurchased && data.name !== 'Custom' ? 'bg-GebetaMain/30' : 'bg-[#202022]'} p-6 h-[420px] rounded-lg`}
+                key={index}>
+                <div className="relative">
+                    <h3 className="font-bold text-lg mb-4">{data.name}</h3>
+                    {data.name !== 'Custom' ? (
+                        <div className="w-5 flex gap-x-2">
                         <span
                             className={`text-xs font-bold ${isPurchased ? "text-white" : "text-zinc-300"} `}>ETB</span>
-                        <h1 className="font-bold text-3xl">{data.price}</h1>
-                    </div>
-                ) : (
-                    <div className="flex flex-col space-y-1">
+                            <h1 className="font-bold text-3xl">{data.price}</h1>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col space-y-1">
                         <span className="w-fit bg-white text-zinc-800 text-xs font-semibold px-2 py-1 rounded mb-2">
                             Recommended
                         </span>
-                        <span className="text-sm font-semibold">Enterprise</span>
-                    </div>
+                            <span className="text-sm font-semibold">Enterprise</span>
+                        </div>
 
-                )}
-                {data.name !== 'Custom' && <span
-                    className="absolute top-1 right-2 bg-green-600 inline-block px-2 py-1 rounded text-xs font-semibold">
+                    )}
+                    {data.name !== 'Custom' && <span
+                        className="absolute top-1 right-2 bg-green-600 inline-block px-2 py-1 rounded text-xs font-semibold">
                     {data.expiredIn} days
                 </span>}
-                <hr className={`${data.name !== 'Custom' ? "mt-3" : "mt-6"} mb-3 border-gray-600`}/>
-            </div>
+                    <hr className={`${data.name !== 'Custom' ? "mt-3" : "mt-6"} mb-3 border-gray-600`}/>
+                </div>
 
-            <div className="flex-grow">
-                <ul className="space-y-1">
-                    {data.included_call_types.map((feature, i) => (
-                        <li className="flex items-center space-x-1 text-xs py-1" key={i}>
-                            <FaCheck className="block text-sm text-green-400 mr-2 shrink-0"/>
-                            <span
-                                className='font-medium text-sm'>{data.call_caps[i] || '0'} {feature.charAt(0).toUpperCase() + feature.slice(1).toLowerCase()} calls</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                <div className="flex-grow">
+                    <ul className="space-y-1">
+                        {data.included_call_types.map((feature, i) => (
+                            <li className="flex items-center space-x-1 text-xs py-1" key={i}>
+                                <FaCheck className="block text-sm text-green-400 mr-2 shrink-0"/>
+                                <span
+                                    className='font-medium text-sm'>{data.call_caps[i] || '0'} {feature.charAt(0).toUpperCase() + feature.slice(1).toLowerCase()} calls</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            <div className="mt-2">
-                {data.name != "Custom" ? <button
-                        className={`w-full transition duration-150 border border-zinc-700 borer-2 outline-none ${isPurchased ? 'bg-white text-zinc-950 border-none' : 'hover:bg-GebetaMain'} hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium`}
-                        onClick={handleUpgrade}
-                        disabled={isPurchased}
-                    >
-                        {isPurchased ? `Selected Plan` : isLogin ? 'Upgrade' : 'Select Plan'}
-                    </button>
-                    :
-                    <button
-                        className='w-full transition duration-150 border border-gray-600 outline-none hover:bg-GebetaMain hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium'
-                        onClick={handleUpgrade}
-                    >
-                        Contact Us
-                    </button>
-                }
+                <div className="mt-2">
+                    {data.name != "Custom" ? <button
+                            className={`w-full transition duration-150 border border-zinc-700 borer-2 outline-none ${isPurchased ? 'bg-white text-zinc-950 border-none' : 'hover:bg-GebetaMain'} hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium`}
+                            onClick={() => handleUpgrade(data.id)}
+                            disabled={isPurchased}
+                        >
+                            {isPurchased ? `Selected Plan` : isLogin ? 'Upgrade' : 'Select Plan'}
+                        </button>
+                        :
+                        <button
+                            className='w-full transition duration-150 border border-gray-600 outline-none hover:bg-GebetaMain hover:border-GebetaMain rounded-md px-3 py-1.5 text-sm font-medium'
+                            onClick={handleUpgrade}
+                        >
+                            Contact Us
+                        </button>
+                    }
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
