e0440219a4d1bb43befb60bbe2017879f52f2e65
zubeyranwar <zubeyranwar@gmail.com>
2024-09-25 13:21:30 +0300

    feat: add the banner component(countdown)

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -8,19 +8,21 @@ import Solutions from "./Solutions";
 import Footer from "../../component/Footer/Footer";
 import Hero from "./Hero";
 import SVGAnimation from "./SVGAnim";
+import Banner from './Banner'
 
 function Main() {
   return (
-    <div className="flex flex-col bg-Dark p-4 md:p-0">
-      <div className="flex flex-col w-full items-center relative">
-        <SVGAnimation />
-        <Hero />
+      <div className="flex flex-col bg-Dark p-4 md:p-0">
+          <div className="flex flex-col w-full items-center relative">
+              <SVGAnimation/>
+              <Hero/>
+          </div>
+          <Banner />
+          <OurCustomers className="flex flex-col md:flex-row"/>
+          <Solutions className="flex flex-col md:flex-row"/>
+          <Testimonials className="flex flex-col md:flex-row"/>
+          <Contact className="flex flex-col md:flex-row"/>
       </div>
-      <OurCustomers className="flex flex-col md:flex-row" />
-      <Solutions className="flex flex-col md:flex-row" />
-      <Testimonials className="flex flex-col md:flex-row" />
-      <Contact className="flex flex-col md:flex-row" />
-    </div>
   );
 }
 
